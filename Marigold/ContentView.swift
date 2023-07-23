390fdc0e11ff030f858a256b615728bb8aa3f797
Kevin Barnes <kevin@donniebarnes.com>
2023-07-23 19:52:37 -0400

    Initial Commit

---
+++ Marigold/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Marigold
+//
+//  Created by Kevin Barnes on 7/23/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Marigold/MarigoldApp.swift
@@ -0,0 +1,17 @@
+//
+//  MarigoldApp.swift
+//  Marigold
+//
+//  Created by Kevin Barnes on 7/23/23.
+//
+
+import SwiftUI
+
+@main
+struct MarigoldApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
