4da01049852eb20dc3c1813493028e547dac4faa
Amodh Dhakal <adhakal2@ncsu.edu>
2024-02-29 16:05:53 -0500

    Changed file structure based on the feedback

--- Marigold/View/ClubCardView.swift
+++ Marigold/View/ClubCardView.swift
@@ -15,10 +15,16 @@ struct ClubCardView: View {
     }
     
     var body: some View {
-        VStack(alignment: .leading){
-            Image(.adcImageLg).resizable().scaledToFill().frame(width: 357, height: 168).clipped()
+        VStack(alignment: .leading) {
+            // Club Banner
+            Image(.adcImageLg)
+                .resizable()
+                .scaledToFill()
+                .frame(width: 357, height: 168)
+                .clipped()
             
             HStack(alignment: .top) {
+                // Club Logo
                 Image(.adcImageSm)
                     .resizable()
                     .scaledToFill()
@@ -30,28 +36,55 @@ struct ClubCardView: View {
                 
                 VStack {
                     HStack {
-                        Text(club.fullName).font(.system(size: 20.0)).fontWeight(.bold).padding([.top], 8.0)
+                        // Club Name
+                        Text(club.fullName)
+                            .font(.system(size: 20.0))
+                            .fontWeight(.bold)
+                            .padding([.top], 8.0)
+                        
                         Spacer()
                     }
+                    
                     HStack {
-                        Text("Vice President").font(.system(size: 12.0))
-                        Button(action: {}, label: {Text("Create Event").fontWeight(/*@START_MENU_TOKEN@*/.bold/*@END_MENU_TOKEN@*/).font(.system(size: 12.0)).foregroundColor(.purple)
-                            .frame(width: 104, height: 26)}).background(.opacity(0.1)).clipShape(.capsule)
-                        Button(action: {}, label: {Text("Create Post").fontWeight(.bold).font(.system(size: 12.0)).foregroundColor(.purple)
-                            .frame(width: 104, height: 26)}).background(.opacity(0.1)).clipShape(.capsule)
+                        // Club Role
+                        Text("Vice President")
+                            .font(.system(size: 12.0))
+                        
+                        // Create Event Button
+                        Button(action: {}, label: {
+                            Text("Create Event")
+                                .fontWeight(/*@START_MENU_TOKEN@*/.bold/*@END_MENU_TOKEN@*/)
+                                .font(.system(size: 12.0))
+                                .foregroundColor(.purple)
+                                .frame(width: 104, height: 26)
+                        })
+                        .background(.opacity(0.1))
+                        .clipShape(.capsule)
+                        
+                        // Create Post Button
+                        Button(action: {}, label: {
+                            Text("Create Post")
+                                .fontWeight(.bold)
+                                .font(.system(size: 12.0))
+                                .foregroundColor(.purple)
+                            .frame(width: 104, height: 26)})
+                        .background(.opacity(0.1))
+                        .clipShape(.capsule)
+                        
                         Spacer()
                     }
+                    
                     Spacer()
                 }
             }
-        }
-        .cornerRadius(10.0).frame(width: 359, height: 264)
-        .background(
-            RoundedRectangle(cornerRadius: 20)
-                .fill(Color.white)
-                .shadow(color: .gray.opacity(0.5), radius: 3)
-                .mask(RoundedRectangle(cornerRadius: 50.0).padding(.bottom, -20))
-        )
+        }.cornerRadius(10.0)
+            .frame(width: 359, height: 264)
+            .background(
+                RoundedRectangle(cornerRadius: 20)
+                    .fill(Color.white)
+                    .shadow(color: .gray.opacity(0.5), radius: 3)
+                    .mask(RoundedRectangle(cornerRadius: 50.0).padding(.bottom, -20))
+            )
         
     }
 }
